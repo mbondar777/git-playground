7476400e4f8ca0062357b107fd3c7a5b73777909
mbondar777 <mbondar@kse.org.ua>
2024-10-31 15:20:52 +0200

    if errors == ERRORS_TO_LOSE:             print('You lose')             exit()

--- main.py
+++ main.py
@@ -9,6 +9,16 @@ def congratulate_user():
 def is_game_over():
     return guessed == WORDS_TO_WIN or errors == ERRORS_TO_LOSE
 
+def guess_is_valid(candidate):
+    for letter in candidate:
+        if letter not in word:
+            print(f"You can not use letter {letter}" )
+            return False
+        count = word.count(letter)
+        if count < candidate.count(letter):
+            print(f"You can use letter {letter} only {count} times")
+        return False
+    return True
 
 guessed = 0
 errors = 0
@@ -37,4 +47,8 @@ while not is_game_over():
         print(f"That's right! {WORDS_TO_WIN - guessed} to go")
     else:
         errors += 1
+        if errors == ERRORS_TO_LOSE:
+            print('You lose')
+            exit()
         print(f"Oops :( No such word, you have {ERRORS_TO_LOSE - errors} lives more")
+
